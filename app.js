f4484f22cc5888668dd140b3565be38daf4e2efb
sahilsawant952001 <sahil.sawant3@spit.ac.in>
2021-07-07 20:45:49 +0530

    removed mongoose connection url

--- app.js
+++ app.js
@@ -1,18 +1,14 @@
 const express = require("express");
 const app = express();
-const mongoose = require("mongoose");
 const cors = require('cors');
 
 const authRoutes = require('./API/Routes/Auth');
 const userRoutes = require('./API/Routes/User');
 
-mongoose.connect("mongodb://localhost/jpmc",{useNewUrlParser:true ,useCreateIndex:true ,useUnifiedTopology:true});
-
 app.use(express.json());
 app.use(express.urlencoded({extended:true}));
 app.use(cors());
 
-// Routes which should handle requests
 app.use('/auth',authRoutes);
 app.use('/user',userRoutes);
 
